5d078d73d9764beaf89677bcdeb223453b50f850
Machu-Pichu1021 <195713740+MachuPichu1021@users.noreply.github.com>
2025-07-19 15:56:24 +0900

    Finished Basic Character Controller
    Wall Running/Jumping
    Sliding

--- Grappling Hook Game/Assets/Scripts/PlayerController.cs
+++ Grappling Hook Game/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private Collider normalColl;
+    private Collider slidingColl;
 
     [SerializeField] private Camera cam;
     private float pitch = 0;
@@ -16,9 +18,16 @@ public class PlayerController : MonoBehaviour
     private const float jumpPower = 7.5f;
     [SerializeField] private Transform groundCheckPoint;
 
+    [SerializeField] private Transform leftWallCheckPoint;
+    [SerializeField] private Transform rightWallCheckPoint;
+
+    private bool isSliding;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        normalColl = GetComponent<CapsuleCollider>();
+        slidingColl = GetComponent<SphereCollider>();
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -26,28 +35,60 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        bool isOnGround = IsOnGround();
+        bool isOnWall = IsOnWall();
+
         transform.Rotate(Vector3.up * Input.GetAxis("Mouse X"));
 
-        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (input.magnitude > 1) input = input.normalized;
-        Vector3 velocity = (input.x * transform.right + input.y * transform.forward) * speed;
-        rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
+        Vector2 input = Vector2.zero;
+        if (!isSliding)
+        {
+            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (input.magnitude > 1) input = input.normalized;
+            Vector3 velocity = (input.x * transform.right + input.y * transform.forward) * speed;
+            rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
 
-        if (input.y > 0) speed += 2.5f * Time.deltaTime;
-        else speed -= 5 * Time.deltaTime;
-        speed = Mathf.Clamp(speed, 5, 15);
+            if (input.y > 0) speed += 2.5f * Time.deltaTime;
+            else speed -= 5 * Time.deltaTime;
+            speed = Mathf.Clamp(speed, 5, maxSpeed);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround()) rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            if (Input.GetKeyDown(KeyCode.Space) && (isOnGround || isOnWall)) rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
 
         pitch -= Input.GetAxis("Mouse Y");
         pitch = Mathf.Clamp(pitch, -60, 60);
-        roll = -5 * input.x;
+        roll = -7.5f * input.x;
+        if (isOnWall) roll *= -1;
         cam.transform.localRotation = Quaternion.Euler(pitch, 0, roll);
         cam.fieldOfView = 60 + 3 * speed;
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && speed > 10)
+        {
+            speed += 10;
+            isSliding = true;
+            normalColl.enabled = false;
+            slidingColl.enabled = true;
+        }
+        if (isSliding)
+        {
+            speed -= 10 * Time.deltaTime;
+            if (speed < 7.5f)
+            {
+                isSliding = false;
+                normalColl.enabled = true;
+                slidingColl.enabled = false;
+            }
+        }
     }
 
-    private bool isOnGround()
+    private bool IsOnGround()
     {
         return Physics.Raycast(groundCheckPoint.position, Vector3.down, 0.05f);
+    }
+
+    private bool IsOnWall()
+    {
+        return ((Physics.Raycast(leftWallCheckPoint.position, -transform.right, 0.05f) && Input.GetKey(KeyCode.A))
+            || (Physics.Raycast(rightWallCheckPoint.position, transform.right, 0.05f) && Input.GetKey(KeyCode.D))) && !IsOnGround();
     }
 }
